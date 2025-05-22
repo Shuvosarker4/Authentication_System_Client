acfa26c4afc587ed5c07504c67d0465e4a8ce842
Shuvo Sarker <Shuvo01797451@gmail.com>
2025-05-22 10:24:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl text-green-400">this is my project</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router";
+
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-base-200 flex items-center justify-center">
+      <div className="text-center space-y-6 p-10">
+        <h1 className="text-5xl font-bold text-primary">
+          Welcome to AuthHub 🔐
+        </h1>
+        <p className="text-lg text-base-content">
+          Secure authentication system with email activation and JWT support.
+        </p>
+        <div className="flex justify-center gap-4">
+          <Link to="/login" className="btn btn-primary">
+            Login
+          </Link>
+          <Link to="/register" className="btn btn-outline btn-secondary">
+            Register
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
